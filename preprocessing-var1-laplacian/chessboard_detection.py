5de1991e3dfc98076fded9114fea395bd9092a30
martinverr <martinverrascina@gmail.com>
2023-05-03 00:09:43 +0200

    bilateral inserted in var 01

--- preprocessing-var1-laplacian/chessboard_detection.py
+++ preprocessing-var1-laplacian/chessboard_detection.py
@@ -16,25 +16,38 @@ def find_board(fname, output_name, verbose_show=False, verbose_output=False):
     assert img is not None
     
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+    blurred = cv2.blur(gray, (3,3))
     if verbose_show:
-        cv2.imshow("blurred grey img", gray)
+        cv2.imshow("blurred grey img", blurred)
         cv2.waitKey(0)
 
     # Canny edge detection
     sigma=0.20
-    edges = auto_canny(gray, sigma=sigma)    
+    edges = auto_canny(blurred, sigma=sigma)    
     if verbose_show:
         cv2.imshow(f"cannied {sigma}", edges)
         cv2.waitKey(0)
     
     # Apply Laplace function
-    edges2 = cv2.Laplacian(gray, ddepth = cv2.CV_16S, ksize=3)
+    edges2 = cv2.Laplacian(blurred, ddepth = cv2.CV_16S, ksize=3)
     edges2 = cv2.convertScaleAbs(edges2)
+    edges2 = cv2.threshold(edges2, 127,255,cv2.THRESH_BINARY)[1]
     if verbose_show:
         cv2.imshow("laplacian", edges2)
         cv2.waitKey(0)
 
+    #bilateral
+    edges3 = cv2.bilateralFilter(gray,9,75,75)
+    if verbose_show:
+        cv2.imshow("bilateral", edges3)
+        cv2.waitKey(0)
+    sigma=0.05
+    edges3 = auto_canny(edges3, sigma=sigma)    
+    if verbose_show:
+        cv2.imshow(f"cannied bilateral", edges3)
+        cv2.waitKey(0)
+
+        
     # Hough line detection
     lines = cv2.HoughLines(edges, 1, np.pi/180, 100)
     chessLines = ChessLines(lines)
@@ -42,22 +55,28 @@ def find_board(fname, output_name, verbose_show=False, verbose_output=False):
     # Hough line detection
     lines2 = cv2.HoughLines(edges2, 1, np.pi/180, 100)
     chessLines2 = ChessLines(lines2)
+    lines3 = cv2.HoughLines(edges3, 1, np.pi/180, 100)
+    chessLines3 = ChessLines(lines3)
 
     imgcopy = img.copy()
     imgcopy2 = img.copy()
+    imgcopy3 = img.copy()
     if verbose_show:
         output_lines(imgcopy, chessLines.lines, (0,0,255))
         output_lines(imgcopy2, chessLines2.lines, (0,0,255))
+        output_lines(imgcopy3, chessLines3.lines, (0,0,255))
         cv2.imshow("lines", imgcopy)
         cv2.waitKey(0)
         cv2.imshow("lines with laplacian", imgcopy2)
+        cv2.waitKey(0)
+        cv2.imshow("lines with bilateral", imgcopy3)
         cv2.waitKey(0)
     
 
 
 
 def main():
-    input_imgs = glob.glob('./input/**')
+    input_imgs = glob.glob('./input/*6*')
     print(input_imgs)
 
     for input_img in input_imgs:
