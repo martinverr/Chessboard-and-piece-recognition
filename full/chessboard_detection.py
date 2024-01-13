b9703903b5d519ea8efeab3cf2d225965a96aff0
martinverr <martinverrascina@gmail.com>
2024-01-13 16:34:47 +0100

    ignore warnings

--- full/chessboard_detection.py
+++ full/chessboard_detection.py
@@ -5,8 +5,8 @@ import cv2
 from ChessLinesClustering import ChessLines
 from chessboard_detection_functions import *
 
-from ChessLinesClustering import ChessLines
-from chessboard_detection_functions import *
+import warnings
+warnings.filterwarnings("ignore")
 
 def board_detection(fname, output_name, verbose_show=False, verbose_output=False):
     """
